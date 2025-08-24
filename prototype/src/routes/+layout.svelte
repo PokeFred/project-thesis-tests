989b864fb5599217f486fe8347655e99b9e63aae
PokeFred <cedric.paelmke@gmx.de>
2025-08-25 00:05:07 +0200

    chore: update prototype

--- prototype/src/routes/+layout.svelte
+++ prototype/src/routes/+layout.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
     import "../app.css"
     import type { LayoutProps } from "./$types"
+    import Icon from "svelte-awesome"
+    import { faBars } from "@fortawesome/free-solid-svg-icons/faBars"
 
-    let { children } = $props()
+    let { children }: LayoutProps = $props()
 </script>
 
-{@render children()}
+<div class="mx-auto w-full max-w-xs h-auto border border-black">
+    <div class="w-full h-12 flex justify-between items-center px-4">
+        <span class="text-xl font-bold">Title</span>
+        <Icon data={faBars} class="w-6 h-6" />
+    </div>
+    <div class="w-full h-auto border-t border-black p-4">
+        {@render children()}
+    </div>
+</div>
--- prototype/src/routes/+page.svelte
+++ prototype/src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import Icon from "svelte-awesome"
+    import { faCheck } from "@fortawesome/free-solid-svg-icons/faCheck"
+    import { faClock } from "@fortawesome/free-solid-svg-icons/faClock"
+    import { faKey } from "@fortawesome/free-solid-svg-icons/faKey"
+
+    type State = "LOCKED" | "OPEN" | "FINISHED"
+    type Station = { state: State, text: string }
+    let stations: Station[] = [
+        { state: "FINISHED", text: "Station 01" },
+        { state: "FINISHED", text: "Station 02" },
+        { state: "OPEN", text: "Station 03" },
+        { state: "LOCKED", text: "Station 04" },
+        { state: "LOCKED", text: "Station 05" }
+    ]
+</script>
+
+<div class="w-full h-auto text-lg text-center">Stationen</div>
+<div class="mt-3 w-full h-auto flex justify-center items-center">
+    <div class="mr-3 text-sm shrink-0">Geschafft:</div>
+    <div class="w-full h-4 bg-gray-500/25 rounded-xl">
+        <div class="w-[50%] h-4 bg-gray-500/60 rounded-xl"></div>
+    </div>
+</div>
+<div class="mt-3 w-full h-auto grid grid-cols-1 gap-2">
+    {#each stations as station}
+            {#if station.state === "FINISHED"}
+                <button class="w-full h-auto bg-green-500/15 border-2 border-green-500 rounded-xl grid grid-cols-[24px_auto] gap-4 cursor-default p-4">
+                    <Icon data={faCheck} class="w-6 h-6" />
+                    <div class="text-left">{station.text}</div>
+                </button>
+            {:else if station.state === "OPEN"}
+                <button class="w-full h-auto border border-black rounded-xl grid grid-cols-[24px_auto] gap-4 cursor-pointer p-4">
+                    <Icon data={faClock} class="w-6 h-6" />
+                    <div class="text-left">{station.text}</div>
+                </button>
+            {:else}
+                <button class="w-full h-auto border border-black rounded-xl grid grid-cols-[24px_auto] gap-4 cursor-default opacity-50 p-4">
+                    <Icon data={faKey} class="w-6 h-6" />
+                    <div class="text-left">{station.text}</div>
+                </button>
+            {/if}
+    {/each}
+</div>
