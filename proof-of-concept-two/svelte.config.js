c1af0a983b5ae0aaff83284bc4166636b6a25088
PokeFred <cedric.paelmke@gmx.de>
2025-08-24 18:15:50 +0200

    chore: init proof-of-concept-two

---
+++ proof-of-concept-two/svelte.config.js
@@ -0,0 +1,29 @@
+import adapter from "@sveltejs/adapter-static"
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
+
+/** @type {import("@sveltejs/kit").Config} */
+const config = {
+    preprocess: vitePreprocess(),
+    kit: {
+        adapter: adapter({
+            pages: "./build",
+            assets: "./build",
+            precompress: true
+        }),
+        env: {
+            publicPrefix: "PUBLIC_",
+            privatePrefix: "PRIVATE_"
+        },
+        alias: {
+            $server: "./src/lib/server",
+            $components: "./src/lib/components",
+            $utils: "./src/lib/utils",
+            $images: "./src/lib/images",
+            $stores: "./src/lib/stores",
+            $styles: "./src/lib/styles",
+            $types: "./src/lib/types"
+        }
+    }
+}
+
+export default config
