6eb0d44691551d783ae13cb07dd7b479098372e0
Simon.Malok <simonmalok@gmail.com>
2025-08-24 23:49:00 +0200

    fix: route

--- proof-of-concept-two/src/routes/+page.ts
+++ proof-of-concept-two/src/routes/+page.ts
@@ -2,6 +2,7 @@ import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({fetch}) => {
 	return {
-		cutoutData: await (await fetch("./images/cutouts.json")).json()
+		// cutoutData: await (await fetch("./images/cutouts.json")).json()
+		cutoutData: await (await fetch("https://pokefred.github.io/project-thesis-tests/proof-concept-two/images/cutouts.json")).json()
 	};
 };
