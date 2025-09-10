37b6f8ea82ef66aee949a0f7fe8d5448c14377dd
Simon.Malok <simonmalok@gmail.com>
2025-09-10 18:55:10 +0200

    change: dev redirect

--- prototype/src/routes/+page.ts
+++ prototype/src/routes/+page.ts
@@ -1,6 +1,7 @@
 import { redirect } from "@sveltejs/kit"
 import type { PageLoad } from "./$types"
+import { dev } from '$app/environment';
 
 export const load: PageLoad = async (): Promise<{}> => {
-    redirect(303, "/project-thesis-tests/prototype/app/stations")
+    redirect(303, dev ? "/app/stations" : "/project-thesis-tests/prototype/app/stations")
 }
