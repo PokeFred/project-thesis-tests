02919faae72d834d9cffa833edb0fc39715724cf
Simon.Malok <simonmalok@gmail.com>
2025-09-10 18:34:49 +0200

    change: url

--- prototype/src/routes/app/stations/station_04/raetsel_00/game/+page.ts
+++ prototype/src/routes/app/stations/station_04/raetsel_00/game/+page.ts
@@ -2,7 +2,7 @@ import type { PageLoad } from "../../../../../$types";
 
 export const load: PageLoad = async ({ fetch }) => {
 	// const path = "/Puzzle";
-	const path = "https://pokefred.github.io/project-thesis-tests/proof-concept-two/images";
+	const path = "https://pokefred.github.io/project-thesis-tests/prototype/images";
 	const backgroundSrc = "Image.png";
 	return {
 		path: path,
