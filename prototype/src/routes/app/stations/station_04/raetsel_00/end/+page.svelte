140f56067456fed894328ff15414940750116ef1
Simon.Malok <simonmalok@gmail.com>
2025-09-11 14:41:07 +0200

    add: button styling

---
+++ prototype/src/routes/app/stations/station_04/raetsel_00/end/+page.svelte
@@ -0,0 +1,7 @@
+<p>
+    <!-- Ändern, dass nur Punkte angezeigt werden, wenn man das Rätsel neu gelöst hat. -->
+    <!-- Verdiente Punkte: {score.getLastGained()}  -->
+    Verdiente Punkte: 0 
+</p>
+
+<a href=".." data-sveltekit-preload-data data-sveltekit-preload-code class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Zurück zur Rätselübersicht</a>
