88ae8c73be32f5630596bc31cc0ce43ebd234bab
Simon.Malok <simonmalok@gmail.com>
2025-10-03 22:32:59 +0200

    add: anziehspiel

--- prototype/src/routes/app/stations/station_04/raetsel_00/custom_shape_test/+page.svelte
+++ prototype/src/routes/app/stations/station_04/raetsel_00/custom_shape_test/+page.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
     import PuzzleGame from "$components/PuzzleGame/PuzzleGame.svelte";
+    import type { PageProps } from "./$types"
 
+    const { data }: PageProps = $props()
+
+    let complete: () => void = $state(()=>{})
 
 </script>
 
-<PuzzleGame />
+<PuzzleGame path={data.path} backgroundSrc={data.backgroundSrc} puzzleData={data.puzzleData} alt={"Puzzel Spiel"} bind:complete={complete}  />
