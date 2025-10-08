07b4ea918c35096909258a2fdac89dbd11e43161
Simon.Malok <simonmalok@gmail.com>
2025-10-08 22:45:11 +0200

    add: ShowMore Component

---
+++ prototype/src/lib/components/ShowMore.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import type { Snippet } from "svelte";
+
+    const { base, extension } : {base: Snippet, extension: Snippet} = $props();
+    let shown: boolean = $state(false);
+
+    function onclick(): void {
+        shown = !shown;
+    }
+
+</script>
+
+<div>
+    {@render base?.()}
+    {#if shown}
+        {@render extension?.()}
+    {/if}
+    <button {onclick}>{shown ? "Weniger Anzeigen" : "Mehr Anzeigen"}</button>
+</div>
+
+<!-- Beispiel: -->
+
+<!-- 
+<ShowMore>
+    {#snippet base()}
+        <p>Basistext</p>
+    {/snippet}
+    {#snippet extension()}
+        <p>Zusatztext</p>
+    {/snippet}
+</ShowMore> 
+-->
