244eec40aa11ae5103284d598e2ad0823bdefedb
Simon Malok <simonmalok@gmail.com>
2025-10-13 22:07:53 +0200

    add: Select Component

---
+++ prototype/src/lib/components/Select.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+
+    let { options }: { options: string[]} = $props();
+
+    let selected: HTMLButtonElement | undefined = undefined;
+    let optionButtons: HTMLButtonElement[];
+
+    let show: boolean = $state(false);
+
+    function onclick(): void {
+        show = !show;
+    }
+</script>
+
+<!-- größe des anzeige buttons so groß wie breite content selbst/optionen -->
+<div class="relative inline-block">
+    <button {onclick} class="cursor-pointer px-1 border-1 border-black rounded">Dropdown</button>
+    <div class="absolute {show ? "block" : "hidden"} w-full bg-white rounded shadow-xl z-10">
+        {#each options as option }
+            <button class="block w-full cursor-pointer hover:bg-blue-500 hover:text-white">{option}</button>
+        {/each}
+    </div>
+</div>
