b67ebf407d4727fc3966861a7a1f0229f7ead70c
Simon Malok <simonmalok@gmail.com>
2025-10-14 23:01:35 +0200

    add: AssignmentGame

---
+++ prototype/src/routes/old/app/stations/station_04/raetsel_0000/introduction/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+    import { faAngleLeft } from "@fortawesome/free-solid-svg-icons/faAngleLeft";
+    import { faSquareCheck } from "@fortawesome/free-regular-svg-icons";
+    import { faXmark } from "@fortawesome/free-solid-svg-icons";
+    import { Icon } from "svelte-awesome";
+
+</script>
+<div class="w-full h-auto flex justify-between items-center">
+    <button onclick={(): Promise<void> => goto("..")} class="w-8 h-8 border border-black rounded-lg flex justify-center items-center cursor-pointer">
+        <Icon data={faAngleLeft} />
+    </button>
+    <div class="text-lg underline">Rätsel 0000</div>
+    <div></div>
+</div>
+
+<p class="my-4 w-full h-auto text-sm">
+    Ordne den Zünften das richtige Handwerk zu. Aber Achtung: es gibt nur <strong>sieben richtige
+    Paare!</strong> Es wurden <strong>drei Zünfte dazu gemischt.</strong> Schaffst Du es, die <strong>richtigen Paare</strong> zu
+    finden?
+</p>
+
+<p class="my-4 w-full h-auto text-sm">
+    <strong>Zünfte:</strong> Schmied, Fleischer, Bäcker, Bader, Tuchmacher, Gerber, Schuhmacher, Krämer,
+    Fettkrämer, Abdecker
+</p>
+
+<p class="my-4 w-full h-auto text-sm">
+    <strong>Handwerk:</strong> Metallverarbeitung, Getreideverarbeitung, Fleischverarbeitung,
+    Lederverarbeitung, Feinlederverarbeitung, Gemischtwarenhandel, Handel mit Butter, Öl,
+    Speck etc.
+</p>
+
+
+<section class="my-4 w-full h-auto text-sm">
+    <h2 class="underline">Punkteverteilung:</h2>
+    <ul>
+        <li><Icon data={faSquareCheck} class="text-green-500 mr-2"/>Richtige Antwort: +10 Münzen</li>
+        <li><Icon data={faXmark} class="text-red-500 mr-2"/>Falsche Antwort: -3 Münzen</li>
+    </ul>
+</section>
+
+<div class="mt-3">
+    <a href="./game" data-sveltekit-preload-data data-sveltekit-preload-code class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Rätsel Starten</a>
+</div>
