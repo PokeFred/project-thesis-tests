dc477214886da41e56bcb04fba99d8cdaf7f9b5f
Simon Malok <simonmalok@gmail.com>
2025-10-14 22:44:57 +0200

    fix: lineheight

--- prototype/src/lib/components/Select.svelte
+++ prototype/src/lib/components/Select.svelte
@@ -18,7 +18,7 @@
 
 <!-- w-[{width}px] -->
 <div class="relative inline-block" style:width={`${width}px`}>
-    <button bind:this={selected} onclick={showMenu} class="cursor-pointer w-full min-h-5 px-1 border-1 border-black rounded" style:width={`${width}px`}></button>
+    <button bind:this={selected} onclick={showMenu} class="cursor-pointer w-full min-h-5 border-1 border-black rounded" style:width={`${width}px`}><span class="invisible">A</span></button>
     <div bind:this={optionMenu} class="absolute block {show ? "visible" : "invisible"} bg-white rounded shadow-xl z-10" >
         {#each options as option }
             <button onclick={()=>selectOption(option)} class="block w-full cursor-pointer hover:bg-blue-500 hover:text-white" >{option}</button>
