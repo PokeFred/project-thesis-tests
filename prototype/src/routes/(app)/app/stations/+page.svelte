d05357979a0bc371a558bed789bfef41da6eb20e
PokeFred <cedric.paelmke@gmx.de>
2025-10-27 16:50:58 +0100

    update

--- prototype/src/routes/(app)/app/stations/+page.svelte
+++ prototype/src/routes/(app)/app/stations/+page.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
+    import type { PageProps } from "./$types"
     import { goto } from "$app/navigation"
-    import game from "$stores/game"
     import Icon from "svelte-awesome"
     import { faAngleLeft } from "@fortawesome/free-solid-svg-icons/faAngleLeft"
     import { faCheck } from "@fortawesome/free-solid-svg-icons/faCheck"
     import { faClock } from "@fortawesome/free-solid-svg-icons/faClock"
     import { faKey } from "@fortawesome/free-solid-svg-icons/faKey"
 
-    import { config, getPuzzleState, getPuzzleScore } from "../game"
+    let { data }: PageProps = $props()
 </script>
 
 <button onclick={() => goto("/app")} class="mb-3 w-8 h-8 text-black border border-black rounded-lg flex justify-center items-center cursor-pointer">
@@ -18,28 +18,28 @@
     <div class="mt-3 w-full h-auto flex justify-center items-center">
         <div class="mr-3 text-sm shrink-0">Geschafft:</div>
         <div class="w-full h-auto bg-gray-500/25 rounded-xl">
-            <div class="w-[{Math.floor($game.score.current * 100 / $game.score.max)}%] h-4 bg-gray-500/60 rounded-xl"></div>
+            <div class="w-[{data.completion}%] h-4 bg-blue-500 rounded-xl"></div>
         </div>
     </div>
     <div class="mt-3 w-full h-auto grid grid-cols-1 gap-2">
-        {#each config.stations as station}
-            {#if getPuzzleState(station.identifier) === "FINISHED"}
+        {#each data.stations as station}
+            {#if station.state === "FINISHED"}
                 <div class="w-full h-auto bg-green-500/15 border-2 border-green-500 rounded-xl grid grid-cols-[24px_auto_48px] gap-4 cursor-default p-4">
                     <Icon data={faCheck} class="w-6 h-6" />
                     <span class="text-left">{station.name}</span>
-                    <span class="text-right">{getPuzzleScore(station.identifier)}/{station.score}</span>
+                    <span class="text-right">{station.score.current}<span class="mx-0.5">/</span>{station.score.max}</span>
                 </div>
-            {:else if getPuzzleState(station.identifier) === "OPEN"}
+            {:else if station.state === "OPEN"}
                 <button onclick={(): Promise<void> => goto(`./stations/${station.identifier}`)} class="w-full h-auto border border-black rounded-xl grid grid-cols-[24px_auto_48px] gap-4 cursor-pointer p-4">
                     <Icon data={faClock} class="w-6 h-6" />
                     <span class="text-left">{station.name}</span>
-                    <span class="text-right">{getPuzzleScore(station.identifier)}/{station.score}</span>
+                    <span class="text-right">{station.score.current}<span class="mx-0.5">/</span>{station.score.max}</span>
                 </button>
             {:else}
                 <div class="w-full h-auto border border-black rounded-xl grid grid-cols-[24px_auto_48px] gap-4 cursor-default opacity-50 p-4">
                     <Icon data={faKey} class="w-6 h-6" />
                     <span class="text-left">{station.name}</span>
-                    <span class="text-right">{getPuzzleScore(station.identifier)}/{station.score}</span>
+                    <span class="text-right">{station.score.current}<span class="mx-0.5">/</span>{station.score.max}</span>
                 </div>
             {/if}
         {/each}
---
+++ prototype/src/routes/(app)/app/stations/+page.ts
@@ -0,0 +1,30 @@
+import type { PageLoad } from "./$types"
+import config from "$config"
+
+type _Station = {
+    identifier: string,
+    name: string,
+    score: {
+        current: number,
+        max: number
+    },
+    state: string
+}
+
+export const load: PageLoad = async (): Promise<{ completion: number, stations: _Station[] }> => {
+    return {
+        completion: 50,
+        stations: config.stations
+            .map((element: any): _Station => {
+                return {
+                    identifier: element.identifier,
+                    name: element.stitle,
+                    score: {
+                        current: 0,
+                        max: 1
+                    },
+                    state: "OPEN"
+                }
+            })
+    }
+}
