c95c36058c2fdccabb525725ce93d54cb8e980c5
PokeFred <cedric.paelmke@gmx.de>
2025-10-28 12:40:09 +0100

    update

--- prototype/src/routes/(public)/faq/+page.svelte
+++ prototype/src/routes/(public)/faq/+page.svelte
@@ -1,45 +1,11 @@
 <script lang="ts">
-    import { Icon } from "svelte-awesome"
-    import { faPlus } from "@fortawesome/free-solid-svg-icons/faPlus"
-    import { faMinus } from "@fortawesome/free-solid-svg-icons/faMinus"
+    import type { PageProps } from "./$types"
+    import Accordion from "./FaqAccordion.svelte"
 
-    type AccordionList = AccordionListItem[]
-
-    type AccordionListItem = AccordionQuestion | AccordionSeperator
-
-    type AccordionQuestion = {
-        type: "question",
-        identifier: string,
-        question: string,
-        answer: string,
-        open: boolean
-    }
-
-    type AccordionSeperator = {
-        type: "seperator"
-    }
-
-    const list: AccordionList = $state([
-        { type: "question", identifier: "question_01", question: "Frage 1 ?", answer: "Antwort 1.", open: false },
-        { type: "seperator" },
-        { type: "question", identifier: "question_01", question: "Frage 2 ?", answer: "Antwort 2.", open: false },
-        { type: "seperator" },
-        { type: "question", identifier: "question_01", question: "Frage 3 ?", answer: "Antwort 3.", open: false }
-    ])
+    let { data }: PageProps = $props()
 </script>
 
 <div class="w-full h-auto text-black border border-black rounded-xl p-4">
     <div class="text-xl font-bold underline text-center">FAQ</div>
-     <div class="w-full h-auto grid grid-cols-1 gap-2">
-        {#each list as element}
-            {#if element.type === "question"}
-                <details bind:open={element.open} class="w-full h-auto">
-                    <summary class="w-full h-auto flex justify-between items-center cursor-pointer"><span class="font-bold">{element.question}</span> <Icon data={element.open ? faPlus : faMinus} /></summary>
-                    <div class="px-2 pt-1">{element.answer}</div>
-                </details>
-            {:else}
-                <hr />
-            {/if}
-        {/each}
-     </div>
+    <Accordion list={data.list} />
 </div>
---
+++ prototype/src/routes/(public)/faq/FaqAccordionListQuestion.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { Icon } from "svelte-awesome"
+    import { faPlus } from "@fortawesome/free-solid-svg-icons/faPlus"
+    import { faMinus } from "@fortawesome/free-solid-svg-icons/faMinus"
+
+    let { question, answer }: { question: string, answer: string } = $props()
+    let open: boolean = $state<boolean>(false)
+</script>
+
+<details bind:open={open} class="w-full h-auto">
+    <summary class="w-full h-auto flex justify-between items-center cursor-pointer"><span class="font-bold">{question}</span> <Icon data={open ? faPlus : faMinus} /></summary>
+    <div class="px-2 pt-1">{answer}</div>
+</details>
