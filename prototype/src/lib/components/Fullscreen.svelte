d83ca298f728ce86ffeaf04101d07c2eeb1796c6
Simon.Malok <simonmalok@gmail.com>
2025-10-28 22:19:45 +0100

    change: fullscreen

--- prototype/src/lib/components/Fullscreen.svelte
+++ prototype/src/lib/components/Fullscreen.svelte
@@ -1,4 +1,4 @@
-<!-- Fullscreen mithilfe der Fullscreen API mit pinch to zoom ist nicht gleichzeitig möglich. Deswegen ein selbstgebautes Fullscreen.  -->
+<!-- Fullscreen mithilfe der Fullscreen API mit pinch to zoom ist nicht gleichzeitig möglich. Dialog hat einen margin welches man nicht wegbekommt. Deswegen ein selbstgebautes Fullscreen mithilfe von div.  -->
 <script lang="ts">
     import { type Snippet } from "svelte";
 
@@ -6,7 +6,7 @@
 
     let isFullscreen: boolean = $state(false);
     let button: HTMLButtonElement;
-    let dialog: HTMLDialogElement;
+    let div: HTMLDivElement;
 
     function enableScrolling(): void {
         document.body.style.overflow = "";
@@ -18,24 +18,31 @@
 
     function fullscreen(): void {
         if(isFullscreen) {
-            dialog.showModal();
+            div.style.visibility = "visible"
             disableScrolling();
         }
         else {
-            dialog.close();
+            div.style.visibility = "hidden"
             enableScrolling();
         }
         isFullscreen = !isFullscreen;
     }
 </script>
 
-<!-- TODO: margin an den seiten wegbekommen; Keine scrollbar beim bild -->
-<dialog bind:this={dialog} class="fixed place-self-center bg-black backdrop:bg-black">
-    <button onclick={fullscreen}>
+<div bind:this={div} class="fullscreenDiv fixed w-screen h-screen top-0 left-0 bg-black z-10000 invisible">
+    <button onclick={fullscreen} class="w-full h-full object-contain">
         {@render children()}
     </button>
-</dialog>
+</div>
 
 <button bind:this={button} onclick={fullscreen}  class="block appearance-none touch-none">
     {@render children()}
 </button>
+
+<style>
+    .fullscreenDiv :global(img)  {
+        object-fit: contain;
+        width: 100%;
+        height: 100%;
+    }
+</style>
