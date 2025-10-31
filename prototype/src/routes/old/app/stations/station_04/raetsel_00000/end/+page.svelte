8001798bda6cf95574ca5830a54af822659b8f3b
Simon.Malok <simonmalok@gmail.com>
2025-10-31 19:12:15 +0100

    update

--- prototype/src/routes/old/app/stations/station_04/raetsel_00000/end/+page.svelte
+++ prototype/src/routes/old/app/stations/station_04/raetsel_00000/end/+page.svelte
@@ -1,10 +1,29 @@
 <script lang="ts">
+    import type { GameResultAccordion } from "$components/accordions/GameResultAccordion";
+    import GameResultAccordionComponent from "$components/accordions/GameResultAccordion.svelte";
     import PointSummary from "$components/Games/PointSummary.svelte";
     import { POINTS } from "$lib/State.svelte";
     import { quiz } from "../Quiz"
+
     const selected = quiz.Options.filter((answer, i) => quiz.Selected[i]);
     const answers: string[] = selected.map((answer)=>answer.answer);
     const points: number[] = selected.map((answer)=>answer.correct ? POINTS.ANSWER_CORRECT : POINTS.ANSWER_FALSE);
+
+    const gameResultAccordion: GameResultAccordion = [
+        {
+            type: "question",
+            success: true,
+            question: "(a) Es gab einen Brunnen am Markt.",
+            answers: [
+                {
+                    type: "paragraph",
+                    content: `Richtig: Der Brunnen war zentral für die Wasserversorgung – und ein beliebter
+                             Treffpunkt zum Austausch aller Art. Eine Tafel erinnert heute an den früheren Brunnen.
+                             Findest Du sie?`,
+                }
+            ]
+        }
+    ]
 </script>
 
 <p class="mb-4 w-full h-auto text-sm">
@@ -16,6 +35,6 @@
 
 <PointSummary descriptions={answers} {points} />
 
-<!-- Accordion -->
+<GameResultAccordionComponent accordion={gameResultAccordion}/>
 
 <a href=".." data-sveltekit-preload-data data-sveltekit-preload-code class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">Zurück zur Rätselübersicht</a>
