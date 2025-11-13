acec18d8db2e45e268ebc22e424b019c8a87b159
PokeFred <cedric.paelmke@gmx.de>
2025-11-13 12:52:49 +0100

    update

--- prototype/src/lib/components/PageTransition.svelte
+++ prototype/src/lib/components/PageTransition.svelte
@@ -4,13 +4,13 @@
     import { page } from "$app/state"
 
     let { children }: { children: Snippet } = $props()
-    const x: number = -25
+    const x: number = -10
     const y: number = 0
     const duration: number = 300
 </script>
 
 {#key page.url.pathname}
     <div in:fly={{ x: x, y: y, duration: duration, delay: duration }} out:fly={{ x: x, y: y, duration: duration }}>
-        {@render children?.()}
+        {@render children()}
     </div>
 {/key}
--- prototype/src/routes/(public)/legal/+page.svelte
+++ prototype/src/routes/(public)/legal/+page.svelte
@@ -1,4 +1,24 @@
 <div class="w-full h-auto text-secondary">
     <div class="text-xl font-bold underline text-center">Rechtliches</div>
-    <div class="mt-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi, ex cupiditate. Delectus ratione, optio, sit dolorum aliquid nobis ea minus iusto enim odit magnam. Ea, vel. Voluptatem placeat dolorem sequi.</div>
+    <div class="mt-4 font-bold">Impressum</div>
+    <ul class="list-disc px-6">
+        <li>Digitale-Dienste-Gesetz (DDG)</li>
+        <li>Medienstaatsvertrag (MStV)</li>
+    </ul>
+    <div class="mt-4 font-bold">Cookie-Banner</div>
+    <ul class="list-disc px-6">
+        <li>Telekommunikation-Digitale-Dienste-Datenschutz-Gesetz (TDDDG)</li>
+    </ul>
+    <div class="mt-4 font-bold">Datenschutzerklärung</div>
+    <ul class="list-disc px-6">
+        <li>Datenschutz-Grundverordnung (DSGVO)</li>
+    </ul>
+    <div class="mt-8 font-bold">Relevante Punkte für uns</div>
+    <ul class="list-disc px-6">
+        <li>§25 I TDDDG => Speicherung von App Daten auf dem Gerät des Nutzers (Freiwillige Einwilligung) <span class="text-red-500 italic">[es ist freiwillig die app zu benutzen ?]</span></li>
+        <li>§25 II TDDDG => Ausschlussgrund ? (Daten zum Betrieb zwingend erforderlich) <span class="text-red-500 italic">[JA!!!]</span></li>
+        <li>§5 Ic DSGVO => Geodaten im Backend ? (Grundsatz der Datenminimierung; nur notwendige Daten) <span class="text-red-500 italic">[es wird nur ein boolean im gespeichert, keine Geodaten]</span></li>
+        <li>§5 Ia DSGVO & §13 DSGVO => Transparenz in der Datenschutzerklärung (Wer verarbeitet wie die Daten ?) <span class="text-red-500 italic">[alles im client, nur rätsel/station ergebnisse und user settings]</span></li>
+        <li>Datenverarbeitung durch Drittanbieter Elemente ? (bsp. Landkarten) <span class="text-red-500 italic">[verwenden einfach kein, nur browser native api]</span></li>
+    </ul>
 </div>
