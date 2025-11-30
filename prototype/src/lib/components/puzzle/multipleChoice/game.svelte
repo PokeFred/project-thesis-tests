1f6997ad2a27ed116ac44d6f2de321a07fab7d16
Simon.Malok <simonmalok@gmail.com>
2025-11-30 14:14:49 +0100

    update: multiplechoice

--- prototype/src/lib/components/puzzle/multipleChoice/game.svelte
+++ prototype/src/lib/components/puzzle/multipleChoice/game.svelte
@@ -8,7 +8,6 @@ import type { GameInput, GameOutput } from "./index"
     let { data, setSubmitable }: { data: GameInput, setSubmitable: () => void } = $props()
 
     let output: GameOutput = { answers: [] }
-    // export function getSubmitData(): GameOutput { return output }
 
     export function getSubmitScore(): number {
         return output.answers
@@ -30,27 +29,8 @@ import type { GameInput, GameOutput } from "./index"
 
     const game: MultipleChoice = new MultipleChoice(data.answers.map((answer) => {
         return {answer: answer.text, correct: answer.isCorrect}
-    }) , []);
+    }));
     export const getSubmitData = game.complete.bind(game);
 </script>
 
 <MultipleChoiceComponent multipleChoice={game}/>
-
-<!-- <div class="w-full h-auto border-y-2 border-secondary divide-y-2 divide-secondary flex flex-col">
-    {#each data.answers as element, i}
-        <label class="flex h-[80px] text-[18px] py-2 items-center font-medium">
-            <input onchange={(event: Event): void => select(event.currentTarget as HTMLInputElement, i)} type="checkbox" class="appearance-none touch-manipulation shrink-0 grow-0 w-[32px] h-[32px] ml-[9px] mr-[25px] border-2 border-secondary rounded-full" />
-            <span>{element.text}</span>
-        </label>
-    {/each}
-</div> -->
-
-<style>
-    input[type="checkbox"] {
-        background-color: inherit;
-    }
-
-    input[type="checkbox"]:checked {
-        background-color: var(--color-secondary);
-    }
-</style>
--- prototype/src/lib/components/puzzle/multipleChoice/result.svelte
+++ prototype/src/lib/components/puzzle/multipleChoice/result.svelte
@@ -1,20 +1,8 @@
 <script lang="ts">
-    import type { Result } from "./index"
+    import MultipleChoiceResult from "$components/Games/MultipleChoiceGame/MultipleChoiceResult.svelte";
+    import type { Result } from "$components/Games/MultipleChoiceGame"
 
     let { data }: { data: Result } = $props()
 </script>
 
-<div class="flex flex-col divide-y-2 divide-secondary border-y-2 border-secondary">
-    {#each data.answers as element, i}
-        <div>
-            <div class="flex w-full mt-[25px] mb-[10px]">
-                <div class="grow-0 shrink-0 w-[32px] h-[32px] ml-[9px] mr-[19px] rounded-full border-2 border-secondary {element.isCorrect ? "bg-secondary" : "bg-inherit"}"></div>
-                <div class="flex items-center w-full pl-4 font-bold text-[16px] text-primary bg-secondary rounded-full">Du hast {element.isCorrect ? "RICHTIG" : "FALSCH"} getippt</div>
-            </div>
-            <div class="ml-[66px] font-bold text-[18px]">
-                <span>{element.text}</span>
-                <div class="my-4"></div>
-            </div>
-        </div>
-    {/each}
-</div>
+<MultipleChoiceResult result={data}/>
