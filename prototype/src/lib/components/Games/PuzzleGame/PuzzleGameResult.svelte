1303575122bf8b0812c30dab0179b3757318d378
Simon.Malok <simonmalok@gmail.com>
2025-12-12 20:16:15 +0100

    update

--- prototype/src/lib/components/Games/PuzzleGame/PuzzleGameResult.svelte
+++ prototype/src/lib/components/Games/PuzzleGame/PuzzleGameResult.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
     import Fullscreen from "$components/Fullscreen.svelte";
     import type { Result } from ".";
+    import { POINTS } from "../Quiz";
+    import type { Piece } from "./Puzzle.svelte";
 
     const { result }: { result: Result } = $props();
+    const correctPieces: number = result.placed.reduce((sum: number, piece: Piece) => sum + (piece.Correct ? 1 : 0));
+    const totalPieces: number = result.placed.length;
+    const points: number = correctPieces * POINTS.ANSWER_CORRECT;
+    const totalPoints: number = totalPieces * POINTS.ANSWER_CORRECT;
 </script>
 
 <div class="-mx-4">
@@ -17,6 +23,6 @@
 </div>
 
 <p class="flex justify-between items-center h-8 px-8 -mx-4 mt-7 bg-secondary text-primary font-bold">
-    <span>Du hast ... von ... Teilen richtig</span>
-    <span>.../...</span>
+    <span>Du hast {correctPieces} von {totalPieces} Teilen richtig</span>
+    <span>{points}/{totalPoints}</span>
 </p>
