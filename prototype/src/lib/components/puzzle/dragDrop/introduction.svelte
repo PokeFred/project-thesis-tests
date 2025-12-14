ec065173093ff96ca076a9766e0d7163ff23f0a1
Simon.Malok <simonmalok@gmail.com>
2025-12-15 00:28:07 +0100

    margins

--- prototype/src/lib/components/puzzle/dragDrop/introduction.svelte
+++ prototype/src/lib/components/puzzle/dragDrop/introduction.svelte
@@ -4,9 +4,9 @@
     let { data }: { data: Introduction } = $props()
 </script>
 
-<div class="w-full h-auto grid grid-cols-1 gap-2 px-2 py-4">
+<div class="w-full h-auto grid grid-cols-1 gap-2 mx-1 my-8">
     <div class="font-bold">{data.title}</div>
     {#each data.description as element}
-        <div>{element}</div>
+        <div class="my-4">{element}</div>
     {/each}
 </div>
--- prototype/src/lib/components/puzzle/multipleChoice/introduction.svelte
+++ prototype/src/lib/components/puzzle/multipleChoice/introduction.svelte
@@ -4,7 +4,7 @@
     let { data }: { data: Introduction } = $props()
 </script>
 
-<div class="w-full h-auto grid grid-cols-1 gap-4 my-7">
+<div class="w-full h-auto grid grid-cols-1 gap-4 mx-1 my-7">
     {#each data.text as element}
         <div class="mx-2 my-7 font-bold">{element}</div>
     {/each}
