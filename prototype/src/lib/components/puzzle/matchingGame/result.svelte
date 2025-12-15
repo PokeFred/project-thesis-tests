83de31764fff25560aa84911af09b87360a43382
Simon.Malok <simonmalok@gmail.com>
2025-12-15 01:15:52 +0100

    matching game result

--- prototype/src/lib/components/puzzle/matchingGame/result.svelte
+++ prototype/src/lib/components/puzzle/matchingGame/result.svelte
@@ -1,7 +1,25 @@
 <script lang="ts">
+    import MatchingGameResult from "$components/Games/MatchingGame/MatchingGameResult.svelte";
     import type { Result } from "./index"
 
     let { data }: { data: Result } = $props()
 </script>
 
-<div>{JSON.stringify(data)}</div>
+<!-- <div>{JSON.stringify(data)}</div> -->
+
+<MatchingGameResult result={
+{
+    leftHeader: "HANDWERK",
+    rightHeader: "ZUNFT",
+    pairs: [
+        { left: "Metall-verarbeitung", right: "Schmied" },
+        { left: "Getreide-verarbeitung", right: "Bäcker" },
+        { left: "Fleisch-verarbeitung", right: "Fleischer" },
+        { left: "Leder-verarbeitung", right: "Gerber" },
+        { left: "Feinleder-verarbeitung", right: "Schuhmacher" },
+        { left: "Gemischt-warenhandel", right: "Krämer" },
+        { left: "Handel mit Butter, Öl, Speck etc.", right: "Fettkrämer" }
+    ],
+    answers: []
+}} 
+/>
