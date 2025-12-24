4812052899cbf0c6ba646f287dedf0a0c755aa1b
Simon.Malok <simonmalok@gmail.com>
2025-12-24 13:53:16 +0100

    add: errorspotting game

---
+++ prototype/src/lib/components/Games/ErrorSpottingGame/index.d.ts
@@ -0,0 +1,20 @@
+import type { Content } from "../Content";
+import type { Answer } from "./Cloze.svelte"
+
+// TODO
+
+export type Introduction = {
+    text: string[]
+}
+
+export type GameInput = {
+    
+}
+
+export type GameOutput = {
+
+}
+
+export type Result = {
+
+}
