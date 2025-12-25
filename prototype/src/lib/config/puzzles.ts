90b27accb95dbe39eefcd2c67243ca4f74ee7878
Simon.Malok <simonmalok@gmail.com>
2025-12-26 00:12:19 +0100

    update: path

--- prototype/src/lib/config/puzzles.ts
+++ prototype/src/lib/config/puzzles.ts
@@ -170,7 +170,7 @@ const puzzles: Puzzle[] = [
                 description: []
             },
             game: {
-                path: "/station_04/raetsel_00/game"
+                path: "/station_04/raetsel_01/game"
             },
             result: {}
         }
