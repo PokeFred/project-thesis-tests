07fb59ac2426e7a8e528f770091b90185dbcdb3f
Simon.Malok <simonmalok@gmail.com>
2026-01-09 18:41:33 +0100

    update

--- prototype/src/lib/components/Games/Canvas/PuzzleGame/PuzzleGame.svelte
+++ prototype/src/lib/components/Games/Canvas/PuzzleGame/PuzzleGame.svelte
@@ -2,8 +2,6 @@
     import { onMount } from "svelte";
     import PuzzleController, { type CutoutData, type PuzzleData, type SlotGroup } from "./PuzzleController.svelte";
     import type { GameData } from "$components/puzzle/dragDrop";
-    import { Icon } from "svelte-awesome";
-    import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
     const { input }: { input: GameData } = $props();
     const gameInput: GameData = input 
@@ -46,12 +44,8 @@
     export const getSubmitScore = () => puzzleController.Puzzle.score();
 </script>
 
-<!-- <div bind:this={container} class="fixed top-0 left-0 w-full h-full"></div> -->
-<div class="-m-4">
-    <div bind:this={container} class="w-full h-[85vh]"></div>
-</div>
-
-<div class="flex justify-between py-20 border-b-2">
-    <button><Icon data={faAngleLeft} scale={2}/></button>
-    <button><Icon data={faAngleRight} scale={2}/></button>
+<div class="pb-10 border-b-2">
+    <div class="-m-4">
+        <div bind:this={container} class="w-full h-[85vh]"></div>
+    </div>
 </div>
