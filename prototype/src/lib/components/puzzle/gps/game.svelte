709c2aa62ff3ee56f6df4fca6e8aa86ee53efbd9
PokeFred <cedric.paelmke@gmx.de>
2026-01-11 23:08:02 +0100

    update

--- prototype/src/lib/components/puzzle/gps/game.svelte
+++ prototype/src/lib/components/puzzle/gps/game.svelte
@@ -22,14 +22,14 @@
     }
 
     onMount(() => {
-        setSubmitable()
+        //setSubmitable()
     })
 </script>
 
-<button onclick={fetchLocation} class="w-full h-auto text-[20px] font-medium text-left text-primary bg-secondary rounded-full flex justify-center items-center px-8 {!a ? "cursor-pointer active:scale-95" : "opacity-50 cursor-default"}" disabled={a}>Überprüfe meinen Standort</button>
+<button onclick={fetchLocation} class="{a ? "hidden" : ""} w-full h-auto text-[20px] font-medium text-left text-primary bg-secondary rounded-full flex justify-center items-center px-8 {!a ? "cursor-pointer active:scale-95" : "opacity-50 cursor-default"}" disabled={a}>Überprüfe meinen Standort</button>
 <div class="mt-3 grid grid-cols gap-4">
-    <div class="w-full h-auto text-lg text-primary bg-secondary p-4">Achtung! Du bist vermutlich noch nicht am richtigen Standort. Gehe zu den Koordinaten und überprüfe deinen Standort erneut.</div>
-    <div class="w-full h-auto text-lg text-primary bg-secondary p-4">Standort korrekt</div>
+    <div class="hidden w-full h-auto text-lg text-primary bg-secondary p-4">Achtung! Du bist vermutlich noch nicht am richtigen Standort. Gehe zu den Koordinaten und überprüfe deinen Standort erneut.</div>
+    <div class="hidden w-full h-auto text-lg text-primary bg-secondary p-4">Standort korrekt</div>
     <!-- <SuccessBanner message="Standortzugriff verweigert." /> -->
     <!-- <LoadingBanner /> -->
     <!-- <ErrorBanner message="Standortzugriff verweigert." /> -->
