b7ba4edc76000f8e2827c4ea0163e26f45a474b2
Simon.Malok <simonmalok@gmail.com>
2026-02-02 11:43:25 +0100

    add: station 2 rätsel inhalt

--- prototype/src/lib/components/puzzle/wordGuessing/result.svelte
+++ prototype/src/lib/components/puzzle/wordGuessing/result.svelte
@@ -4,21 +4,21 @@
 
     let { result, saving }: { result: ResultData, saving: SavingData } = $props()
 
+    // TODO Cedric: Station 1 Rätsel 1 Result
     result = {
         questions: [
             {
                 hints: [
                     {
                         tag: "img",
-                        src: "/station_02/raetsel_01/game/image_02.png",
-                        alt: "test",
+                        src: "/station_02/raetsel_01/game/image_00.png",
+                        alt: "Tee",
                         caption: ""
                     },
                     {
                         tag: "text",
                         text: {
-                            crossed: "test",
-                            // normal: " + chen"
+                            crossed: "e",
                         }
                     },
                     {
@@ -28,29 +28,144 @@
                         }
                     },  
                     {
-                        tag: "text",
-                        text: {
-                            normal: "&"
-                        }
-                    },
-                    {
-                        tag: "text",
-                        text: {
-                            normal: "+"
-                        }
-                    },
-                    {
                         tag: "img",
-                        src: "/station_02/raetsel_01/game/image_03.png",
-                        alt: "test",
+                        src: "/station_02/raetsel_01/game/image_01.png",
+                        alt: "Western",
                         caption: ""
-                    }
+                    },
+                    {
+                        tag: "text",
+                        text: {
+                            crossed: "tern"
+                        }
+                    },  
                 ],
                 solution: "Tewes",
-                solutionText: "Lösung: Freundschaft (-schaft) + & + Bauer = Freund & Bauer"
+                solutionText: "Lösung: Tee (-e) + Western (-tern) = Tewes"
             }
         ]
     }
+
+    // TODO Cedric: Station 2 Rätsel 2 Result
+    //  result = {
+    //     questions: [
+    //         {
+    //             hints: [
+    //                 {
+    //                     tag: "img",
+    //                     src: "/station_02/raetsel_02/game/image_00.png",
+    //                     alt: "Freundschaft",
+    //                     caption: ""
+    //                 },
+    //                 {
+    //                     tag: "text",
+    //                     text: {
+    //                         crossed: "schaft"
+    //                     }
+    //                 },
+    //                 {
+    //                     tag: "text",
+    //                     text: {
+    //                         normal: "+"
+    //                     }
+    //                 },  
+    //                 {
+    //                     tag: "text",
+    //                     text: {
+    //                         normal: "&"
+    //                     }
+    //                 },
+    //                 {
+    //                     tag: "text",
+    //                     text: {
+    //                         normal: "+"
+    //                     }
+    //                 },
+    //                 {
+    //                     tag: "img",
+    //                     src: "/station_02/raetsel_02/game/image_01.png",
+    //                     alt: "Bauer",
+    //                     caption: ""
+    //                 }
+    //             ],
+    //             solution: "Freund & Bauer",
+    //             solutionText: "Lösung: Freundschaft (-schaft) + & + Bauer = Freund & Bauer"
+    //         }
+    //     ]
+    // }
+
+    // TODO Cedric: Station 2 Rätsel 3 Result
+    // result = {
+    //     questions: [
+    //         {
+    //             hints: [
+    //                 {
+    //                     tag: "img",
+    //                     src: "/station_02/raetsel_03/game/image_00.png",
+    //                     alt: "Betten",
+    //                     caption: ""
+    //                 },
+    //                 {
+    //                     tag: "text",
+    //                     text: {
+    //                         normal: "+"
+    //                     }
+    //                 },  
+    //                 {
+    //                     tag: "img",
+    //                     src: "/station_02/raetsel_03/game/image_01.png",
+    //                     alt: "Hut",
+    //                     caption: ""
+    //                 },
+    //                 {
+    //                     tag: "text",
+    //                     text: {
+    //                         normal: "t"
+    //                     }
+    //                 },  
+    //             ],
+    //             solution: "Betten Hutt",
+    //             solutionText: "Betten + Hut (t) = Betten Hutt"
+    //         }
+    //     ]
+    // }
+
+    // TODO Cedric: Station 2 Rätsel 4 Result
+    // result = {
+    //     questions: [
+    //         {
+    //             hints: [
+    //                 {
+    //                     tag: "img",
+    //                     src: "/station_02/raetsel_04/game/image_00.png",
+    //                     alt: "Rüschen",
+    //                     caption: ""
+    //                 },
+    //                 {
+    //                     tag: "text",
+    //                     text: {
+    //                         normal: "+"
+    //                     }
+    //                 },  
+    //                 {
+    //                     tag: "img",
+    //                     src: "/station_02/raetsel_04/game/image_01.png",
+    //                     alt: "Gebäck",
+    //                     caption: ""
+    //                 },
+    //                 {
+    //                     tag: "text",
+    //                     text: {
+    //                         crossed: "Ge",
+    //                         normal: " + ä=e"
+    //                     }
+    //                 },  
+    //             ],
+    //             solution: "Rüschenbeck",
+    //             solutionText: "Rüschen + (-Ge)b(ä=e)ck = Rüschenbeck"
+    //         }
+    //     ]
+    // }
 </script>
 
 <ResultComponent result={result} saving={saving} />
