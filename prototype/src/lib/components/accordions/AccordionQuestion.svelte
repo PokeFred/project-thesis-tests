fbe780b5d5d1d8150310b4d619aeff1c3a1a8bad
Simon.Malok <simonmalok@gmail.com>
2026-02-02 17:52:45 +0100

    todo

--- prototype/src/lib/components/accordions/AccordionQuestion.svelte
+++ prototype/src/lib/components/accordions/AccordionQuestion.svelte
@@ -3,6 +3,7 @@
     import { Icon } from "svelte-awesome"
     import { faMinus } from "@fortawesome/free-solid-svg-icons/faMinus"
     import { faPlus } from "@fortawesome/free-solid-svg-icons/faPlus"
+    import Fullscreen from "$components/Fullscreen.svelte";
 
     let { question }: { question: AccordionQuestion } = $props()
     let open: boolean = $state<boolean>(false)
@@ -30,9 +31,23 @@
                 <div class="font-medium">{element.text}</div>
             {:else}
                 <figure>
-                    <img src={element.src} alt={element.alt} class="rounded-lg" />
+                    <Fullscreen>
+                        <img src={element.src} alt={element.alt} class="rounded-lg" />
+                    </Fullscreen>
                     <figcaption>{element.caption}</figcaption>
                 </figure>
+                <!-- TODO Cedric: das ist das richtige gestylte bild. caption und src von caption nutzen -->
+                <!-- <figure class="my-10 -mx-6">
+                    <Fullscreen>
+                        <img src={content.src} alt={content.alt} />
+                    </Fullscreen>
+                    {#if content.caption}
+                        <figcaption class="flex flex-col mx-4.5 mt-3 font-medium">
+                            <span class="mb-2 text-[16px] leading-4">{content.caption?.caption}</span>
+                            <span class="uppercase text-[12px] leading-4.5 tracking-[0.72pt]">{content.caption?.src}</span>
+                        </figcaption>
+                    {/if}
+                </figure> -->
             {/if}
         </div>
     {/each}
