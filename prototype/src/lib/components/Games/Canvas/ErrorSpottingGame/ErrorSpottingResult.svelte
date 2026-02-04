0f12372839e17439b93f02fb6c50582ab285ae8b
Simon.Malok <simonmalok@gmail.com>
2026-02-04 18:47:25 +0100

    bugfix

--- prototype/src/lib/components/Games/Canvas/ErrorSpottingGame/ErrorSpottingResult.svelte
+++ prototype/src/lib/components/Games/Canvas/ErrorSpottingGame/ErrorSpottingResult.svelte
@@ -14,7 +14,7 @@
     let container: HTMLDivElement;
     onMount(async ()=>{
         const image: HTMLImageElement = await loadImage(result.path + "/fehler.png");
-        const errorPaths: string[] = await (await fetch(`${result.path}/paths.json`)).json();
+        const errorPaths: string[][] = await (await fetch(`${result.path}/paths.json`)).json();
 
         controller = new Controller(container, image, errorPaths, true);
     });
@@ -33,7 +33,7 @@
     <div bind:this={container} class="w-full h-[80vh]"></div>
 </div>
 
-<p class="flex justify-between items-center h-8 px-8 -mx-4 mt-7 bg-secondary text-primary text-[16px]">
+<p class="flex justify-between items-center min-h-8 px-8 -mx-4 mt-7 bg-secondary text-primary text-[16px]">
     <span class="font-bold">Du hast {saving.correct} von {saving.total} Unterschieden erkannt</span>
     <span class="font-medium">{ACHIEVED_POINTS}/{TOTAL_POINTS}</span>
 </p>
