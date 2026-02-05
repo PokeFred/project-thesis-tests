617a2f880e34914e8c3a78bc4e664857b6f3d6bd
Simon.Malok <simonmalok@gmail.com>
2026-02-05 17:28:36 +0100

    add: groß/kleinschreibung und leerzeichen bei wordguessing ignorieren

--- prototype/src/lib/components/Games/WordGuessingGame/WordGuessing.svelte.ts
+++ prototype/src/lib/components/Games/WordGuessingGame/WordGuessing.svelte.ts
@@ -14,12 +14,16 @@ export default class WordGuessing implements Quiz<SavingData> {
     public get Inputs() { return this.inputs; }
 
     public complete(): SavingData {
-        return { match: this.inputs.map((input: string, i) => input == this.solutions[i]) } satisfies SavingData;
+        return { match: this.inputs.map((input: string, i) => this.isCorrect(i)) } satisfies SavingData;
     }
 
     public score(): number {
         return this.solutions.reduce((sum: number, solution: string, i: number) => {
-            return sum + (solution == this.inputs[i] ? POINTS.ANSWER_CORRECT : POINTS.ANSWER_FALSE);
+            return sum + (this.isCorrect(i) ? POINTS.ANSWER_CORRECT : POINTS.ANSWER_FALSE);
         }, 0);
     }
+
+    private isCorrect(index: number): boolean {
+        return this.solutions[index].toLowerCase().replace(/\s+/g, "") === this.inputs[index].toLowerCase().replace(/\s+/g, "");
+    } 
 }
