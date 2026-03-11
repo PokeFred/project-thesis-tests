fdebe7488f6c5165858c39c80e000666082f08ee
Simon.Malok <simonmalok@gmail.com>
2026-03-11 13:21:01 +0100

    fix: styling

--- prototype/src/lib/components/puzzle/singleChoice/result.svelte
+++ prototype/src/lib/components/puzzle/singleChoice/result.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <ResultComponent result={result} saving={saving} />
-<div class="w-full h-auto px-1 mb-15 font-bold text-[20px] leading-6">
+<div class="w-full h-auto px-1 my-15 font-bold text-[20px] leading-6">
     {#each result.text as element}
         <div>{element}</div>
     {/each}
