d573a37ffa3eee09018f9507b6b6143bfac402f5
Simon.Malok <simonmalok@gmail.com>
2026-03-11 13:28:15 +0100

    fix: styling

--- prototype/src/routes/(app)/p/[id]/+page.svelte
+++ prototype/src/routes/(app)/p/[id]/+page.svelte
@@ -102,7 +102,7 @@
 <div class="w-full h-auto text-secondary">
     <div class="w-full h-auto flex justify-between items-center my-1 pl-6 pr-4">
         <span class="font-medium text-[16px] uppercase tracking-[1.28px]">{data.station.title}</span>
-        <BackButton path={`/s/${data.station.id}`} />
+        <div class="grow-0 shrink-0"><BackButton path={`/s/${data.station.id}`} /></div>
     </div>
     <div class="-mx-4 bg-secondary">
         <div class="mx-4">
