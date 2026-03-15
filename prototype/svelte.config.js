0f3cccc8320df3aff5a56e4237be1a16ee87a582
PokeFred <cedric.paelmke@gmx.de>
2026-03-15 13:43:41 +0100

    update

--- prototype/svelte.config.js
+++ prototype/svelte.config.js
@@ -17,7 +17,7 @@ const config = {
             privatePrefix: "PRIVATE_"
         },
         paths: {
-            //base: (process.env.NODE_ENV === "production") ? "/einkaufsspuren" : ""
+            base: (process.env.NODE_ENV === "production") ? "/" : "/"
         },
         alias: {
             $config: "./src/lib/config",
