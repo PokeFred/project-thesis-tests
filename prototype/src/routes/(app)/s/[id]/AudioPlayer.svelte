da1f39da1c5e7be1a290abb4aaf21b9192e47028
PokeFred <cedric.paelmke@gmx.de>
2026-03-16 22:43:47 +0100

    update

--- prototype/src/routes/(app)/s/[id]/AudioPlayer.svelte
+++ prototype/src/routes/(app)/s/[id]/AudioPlayer.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { asset } from "$utils/url";
+
     let { path }: { path: string } = $props()
 
     let isPlaying: boolean = $state<boolean>(false)
@@ -7,12 +9,16 @@
     }
 </script>
 
+<audio controls autoplay class="">
+    <source src={asset(path)} type="audio/mpeg" />
+</audio>
+
 <div class="w-full h-auto text-primary bg-secondary rounded-full flex justify-start items-center p-2">
-    <button onclick={toggle} class="w-8 h-8 text-secondary bg-primary rounded-full cursor-pointer active:scale-95">
+    <button onclick={toggle} class="w-8 h-8 text-secondary bg-primary rounded-full flex justify-center items-center cursor-pointer active:scale-95">
         {#if isPlaying}
-            <img src="/audio_play.svg" alt="Play Icon" />
+            <img src="/audio_play.svg" alt="Play Icon" class="w-4 h-4 text-secondary" />
         {:else}
-            <img src="/audio_pause.svg" alt="Pause Icon" />
+            <img src="/audio_pause.svg" alt="Pause Icon" class="w-4 h-4 text-secondary" />
         {/if}
     </button>
     <div class="ml-4 font-bold">Hier der Text zum Anhören</div>
