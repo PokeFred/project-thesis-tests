530c7079f7c57b1000b5b3418be62e8234710fa7
PokeFred <cedric.paelmke@gmx.de>
2026-03-27 15:13:09 +0100

    update

--- prototype/src/routes/(app)/p/[id]/+page.svelte
+++ prototype/src/routes/(app)/p/[id]/+page.svelte
@@ -159,7 +159,7 @@
             {#if skipIntroduction}
                 <button onclick={() => { skipIntroduction?.(); skipIntroduction = undefined }} class="w-full h-auto pl-6 text-left text-[20px] font-medium text-primary bg-secondary rounded-full py-2 cursor-pointer active:scale-95">Weiter</button>
             {:else}
-                <button onclick={(): void => data.puzzle.type === "gps-puzzle" || data.puzzle.type === "reflection-question-puzzle" || (textSelect?.isComplete() || matchingGame?.isComplete() || singleChoice?.isComplete() || multipleChoice?.isComplete() || dragDrop?.isComplete() || errorSpotting?.isComplete() || wordGuessing?.isComplete()) ? submit() : modal.openModal()} class="w-full h-auto pl-6 text-left text-[20px] font-medium text-primary bg-secondary rounded-full py-2 cursor-pointer active:scale-95">{(data.puzzle.type !== "gps-puzzle" && data.puzzle.type !== "reflection-question-puzzle") ? "Ergebnis anzeigen" : "Gehe zu den Rätseln"}</button>
+                <button onclick={(): void => data.puzzle.type === "gps-puzzle" || data.puzzle.type === "reflection-question-puzzle" || (textSelect?.isComplete() || matchingGame?.isComplete() || singleChoice?.isComplete() || multipleChoice?.isComplete() || dragDrop?.isComplete() || errorSpotting?.isComplete() || wordGuessing?.isComplete()) ? submit() : modal.openModal()} class="w-full h-auto pl-6 text-left text-[20px] font-medium text-primary bg-secondary rounded-full py-2 cursor-pointer active:scale-95">{(data.puzzle.type !== "gps-puzzle" && data.puzzle.type !== "reflection-question-puzzle") ? "Ergebnis anzeigen" : (data.puzzle.type === "reflection-question-puzzle" ? "Gehe zu den Reflexionsfragen" : "Gehe zu den Rätseln")}</button>
             {/if}
         </div>
     {/if}
