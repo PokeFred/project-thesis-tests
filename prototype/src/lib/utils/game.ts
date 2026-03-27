f97bdaa375198ae0c672ae004b9c8cddc7d03912
PokeFred <cedric.paelmke@gmx.de>
2026-03-27 12:32:45 +0100

    update

---
+++ prototype/src/lib/utils/game.ts
@@ -0,0 +1,26 @@
+import { getScore } from "$stores"
+import Station from "$utils/station"
+import Puzzle from "$utils/puzzle"
+
+export default class Game {
+    public static getScore(): number {
+        return getScore()
+    }
+
+    public static isFinished(): boolean {
+        const stations: Station[] = Station.getAll()
+        const puzzles: Puzzle[] = []
+        for (const station of stations) {
+            puzzles.push(...station.getPuzzles())
+        }
+
+        let finished: boolean = true
+        for (const puzzle of puzzles) {
+            if (!puzzle.isDone()) {
+                finished = false
+            }
+        }
+
+        return finished
+    }
+}
